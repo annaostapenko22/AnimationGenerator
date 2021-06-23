938a8e95ea74c087e3a57e18b7980bdca0aaf493
Anna Ostapenko <kvintabudinvest@gmail.com>
2021-06-23 19:44:50 +0200

    Added basic project version

---
+++ AnimationGenerator/Animation.swift
@@ -0,0 +1,69 @@
+//
+//  Animation.swift
+//  AnimationGenerator
+//
+//  Created by Anna Ostapenko on 23.06.21.
+//
+import Spring
+
+struct Animation {
+    static let animations: [Spring.AnimationPreset] = [
+        .Shake,
+        .Pop,
+        .Morph,
+        .Squeeze,
+        .Wobble,
+        .Swing,
+        .FlipX,
+        .FlipY,
+        .Fall,
+        .SqueezeLeft,
+        .SqueezeRight,
+        .SqueezeDown,
+        .SqueezeUp,
+        .SlideLeft,
+        .SlideRight,
+        .SlideDown,
+        .SlideUp,
+        .FadeIn,
+        .FadeOut,
+        .FadeInLeft,
+        .FadeInRight,
+        .FadeInDown,
+        .FadeInUp,
+        .ZoomIn,
+        .ZoomOut,
+        .Flash
+    ]
+    static let curves: [Spring.AnimationCurve] = [
+        .Spring,
+        .Linear,
+        .EaseIn,
+        .EaseOut,
+        .EaseInOut,
+        .EaseInSine,
+        .EaseOutSine,
+        .EaseInOutSine,
+        .EaseInQuad,
+        .EaseOutQuad,
+        .EaseInOutQuad,
+        .EaseInCubic,
+        .EaseOutCubic,
+        .EaseInOutCubic,
+        .EaseInQuart,
+        .EaseOutQuart,
+        .EaseInOutQuart,
+        .EaseInQuint,
+        .EaseOutQuint,
+        .EaseInOutQuint,
+        .EaseInExpo,
+        .EaseOutExpo,
+        .EaseInOutExpo,
+        .EaseInCirc,
+        .EaseOutCirc,
+        .EaseInOutCirc,
+        .EaseInBack,
+        .EaseOutBack,
+        .EaseInOutBack
+    ]
+}
--- AnimationGenerator/ViewController.swift
+++ AnimationGenerator/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    @IBOutlet var animationView: UIImageView!
+    private var originCoordinate: CGFloat?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        originCoordinate = animationView.frame.origin.x
     }
 
 
+    @IBAction func runAnimation(_ sender: UIButton) {
+//        pulsate(sender: sender)
+//        UIView.animate(
+//            withDuration: 0.5,
+//            delay: 0,
+//            options: [.autoreverse, .repeat],
+//            animations: {
+//                if self.animationView.frame.origin.x == self.originCoordinate {
+//                    self.animationView.frame.origin.x -= 40
+//                } else {
+//                    self.animationView.frame.origin.x += 40
+//                }
+//            }
+//        )
+        pulsate(sender: animationView)
+    }
+    
+    func shakeAnimation() {
+        
+    }
+    
+    func pulsate(sender: UIView) {
+      
+        let animation = CASpringAnimation(keyPath: "opacity")
+
+    }
 }
 
